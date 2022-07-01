bac624f87a9949d097fef6cda50756da1dce032d
rafaelpcosta1 <rpdc.rafael@gmail.com>
2022-06-30 23:11:57 -0300

    feat: add react-router-dom@v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 
 export const App = () => {
   return (
-    <div>
-      Teste
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
-
