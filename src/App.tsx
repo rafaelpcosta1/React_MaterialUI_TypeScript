429958ae0a1115124d220cb51b4a67dcdd2c39f1
rafaelpcosta1 <rpdc.rafael@gmail.com>
2022-07-03 00:22:48 -0300

    feat: add menu lateral

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 /* eslint-disable linebreak-style */
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes';
+import { MenuLateral } from './shared/components';
 import { AppThemeProvider } from './shared/contexts';
 
 export const App = () => {
   return (
     <AppThemeProvider>
       <BrowserRouter>
-        <AppRoutes />
+        <MenuLateral>
+          <AppRoutes />
+        </MenuLateral>
       </BrowserRouter>
     </AppThemeProvider>
   );
