2754ae975cfc418f33ff19e1db52a2fb58e6b825
rafaelpcosta1 <rpdc.rafael@gmail.com>
2022-07-03 02:24:21 -0300

    feat: added responsiveness in side menu

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,15 @@
 import { Button } from '@mui/material';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { useAppThemeContext } from '../shared/contexts';
+import { useAppThemeContext, useDrawerContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
   const { toggleTheme } = useAppThemeContext();
+  const { toggleDrawerOpen } = useDrawerContext();
 
   return (
     <Routes>
       <Route path="/pagina-inicial" element={<Button variant="contained" color="primary" onClick={toggleTheme}>Toggle Theme</Button>} />
+      <Route path="/Menu" element={<Button variant="contained" color="primary" onClick={toggleDrawerOpen}>Menu Lateral</Button>} />
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
